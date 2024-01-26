d42ffe51e1a394c8c92b1216a2c371424470aafa
AstralTurtle <jamesacs329@gmail.com>
2024-01-26 16:41:43 -0500

    finished dice roller :) c++

---
+++ cppdiceroller/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+// built with clion
+int main() {
+    std::cout << "Enter Dice Roll in format 2d6+3: ";
+    std::string input;
+    std::cin >> input;
+    std::cout << std::endl;
+
+    int dind;
+    dind = input.find('d');
+
+    int numDice = std::stoi(input.substr(0, dind));
+//    find mod symbol
+    int mind;
+    mind = input.find('+');
+    if (mind == -1) {
+        mind = input.find('-');
+    }
+    if (mind == -1) {
+        mind = input.length();
+    }
+
+
+    int sides = std::stoi(input.substr(dind + 1, mind - dind - 1));
+
+
+
+
+
+    std::vector<int> rolls(numDice);
+
+    for (int i = 0; i < numDice; i++) {
+        rolls.at(i) = rand() % sides + 1;
+    }
+    int mod = std::stoi(input.substr(mind, input.length() - mind));
+    int sum = 0;
+    for (int i = 0; i < numDice; i++) {
+        sum += rolls.at(i);
+    }
+    sum += mod;
+    std::cout << "Rolls: ";
+    for (int i = 0; i < numDice; i++) {
+        std::cout << rolls.at(i) << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Sum: " << sum << std::endl;
+
+
+    return 0;
+}
+//make class?
